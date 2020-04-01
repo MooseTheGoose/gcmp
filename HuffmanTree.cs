54f7723744178f1608e72943277147079c8a15ad
MooseTheGoose <MooseTheGoose>
2020-04-01 19:19:06 -0400

    Completed initial implementation of Huffman tree

---
+++ HuffmanTree.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gcmp
+{
+    /*
+     * NOTE: Huffman Tree only supports compression of ASCII characters.
+     */
+    public class HuffmanTree
+    {
+        public int symbol;
+        public int occurences;
+        public HuffmanTree left;
+        public HuffmanTree right;
+
+        public HuffmanTree(int symbol, int occurences, HuffmanTree left, HuffmanTree right)
+        {
+            this.symbol = symbol;
+            this.occurences = occurences;
+            this.left = left;
+            this.right = right;
+        }
+
+        public static HuffmanTree Construct(Dictionary<int, int> frequencyTable)
+        {
+            HuffmanTree root;
+            List<HuffmanTree> nodes = new List<HuffmanTree>();
+
+            foreach(KeyValuePair<int, int> pair in frequencyTable)
+            {
+                nodes.Add(new HuffmanTree(pair.Key, pair.Value, null, null));
+            }
+
+            /* Bubble sort dat by occurence, max is first. */
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                for (int j = 0; j < nodes.Count-1-i; j++)
+                {
+                    if (nodes[j].occurences < nodes[j+1].occurences)
+                    {
+                        HuffmanTree temp = nodes[j];
+                        nodes[j] = nodes[j + 1];
+                        nodes[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (int i = nodes.Count - 1; i >= 1; i--)
+            {
+                HuffmanTree left = nodes[i];
+                HuffmanTree right = nodes[i-1];
+
+                nodes.RemoveAt(i);
+                nodes.RemoveAt(i - 1);
+                HuffmanTree newNode = new HuffmanTree(-1, left.occurences + right.occurences, left, right);
+                nodes.Add(newNode);
+
+                for (int j = i-1; j >= 1; j--)
+                {
+                    HuffmanTree temp;
+                    if (nodes[j].occurences < nodes[j - 1].occurences) { break; }
+                    temp = nodes[j];
+                    nodes[j] = nodes[j - 1];
+                    nodes[j - 1] = temp;
+                }
+            }
+
+            root = nodes[0];
+            return root;
+        }
+
+        public int CompressChar(byte[] bits, int currLen, char c)
+        {
+            if (this.symbol == c)
+            {
+                return currLen;
+            }
+            else if(this.symbol < 0)
+            {
+                int recurLen = currLen + 1;
+                bits[currLen] = 0;
+                int finalLen = this.left.CompressChar(bits, recurLen, c);
+
+                if (finalLen == 0)
+                {
+                    bits[currLen] = 1;
+                    return this.right.CompressChar(bits, recurLen, c);
+                }
+
+                return finalLen;
+            }
+
+            return 0;
+        }
+
+        /* TO-DO: Change from string to byte[] */
+        public int[] Compress(string text)
+        {
+            List<int> bits = new List<int>();
+            bits.Add(0);
+            int nbits = 0;
+            byte[] charBits = new byte[256];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int final = CompressChar(charBits, 0, text[i]);
+
+                for (int j = 0; j < final; j++)
+                {
+                    bits[nbits >> 5] |= charBits[j] << (~nbits & 31);
+
+                    nbits++;
+                    if ((nbits & 31) == 0) { bits.Add(0); }
+                }
+            }
+
+            return bits.ToArray();
+        }
+
+        public string Decompress(int[] bits, int decompLength)
+        {
+            StringBuilder builder = new StringBuilder("");
+            HuffmanTree currNode = this;
+            int bitpos = 0;
+
+            for (int i = 0; i < decompLength; i++)
+            {
+                while (true)
+                {
+                    int currBit = bits[bitpos >> 5] >> (~bitpos & 31) & 1;
+
+                    if (currNode.symbol < 0)
+                    {
+                        if (currBit == 1)
+                        {
+                            currNode = currNode.right;
+                        }
+                        else
+                        {
+                            currNode = currNode.left;
+                        }
+                    }
+                    else
+                    {
+                        builder.Append((char)currNode.symbol);
+                        currNode = this;
+                        break;
+                    }
+
+                    bitpos++;
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public static void PrintTree(HuffmanTree tree, int indent)
+        {
+            StringBuilder builder = new StringBuilder("");
+            for (int i = 0; i < indent; i++) { builder.Append(' '); }
+
+            if (tree.symbol < 0)
+            {
+                Console.WriteLine(builder + "(NON-DATA n='" + tree.occurences + "') {");
+                PrintTree(tree.left, indent + 4);
+                PrintTree(tree.right, indent + 4);
+                Console.WriteLine(builder + "}");
+            }
+            else
+            {
+                Console.WriteLine(builder + "(DATA)" + (char)tree.symbol + " " + tree.occurences +  "(/DATA)");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace gcmp
 {
@@ -6,7 +8,30 @@ namespace gcmp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = "THIS IS A STRING";
+            int[] compressedBits;
+            Dictionary<int, int> freqTable = new Dictionary<int, int>();
+
+            Debug.Assert(args.Length > 0);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int ch = str[i];
+
+                if (freqTable.ContainsKey(ch))
+                {
+                    freqTable[ch]++;
+                }
+                else
+                {
+                    freqTable[ch] = 1;
+                }
+            }
+
+            HuffmanTree root = HuffmanTree.Construct(freqTable);
+            HuffmanTree.PrintTree(root, 0);
+            compressedBits = root.Compress(str);
+            Console.WriteLine(root.Decompress(compressedBits, str.Length));
         }
     }
 }
